9df50b96143b93b62f3cb4064349201153170610
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-04 17:40:25 +0100

    added validation on inpts for manager

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const emailValidation = (answer) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  if(!emailRegex.test(answer)) {
-      return "You have to provide a valid email address!"
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(answer)) {
+    return "You have to provide a valid email address!";
   }
-  return true
-}
+  return true;
+};
 
 const roles = [
   {
@@ -23,15 +23,31 @@ const teamQuestions = [
     type: "input",
     name: "Manager's Name",
     message: "Please provide a team manager's name",
-  },
-  {
-    type: "number",
-    name: "Manager's ID",
-    message: "Please enter employee ID",
+    validate: nameInput => {
+      if (nameInput) {
+          return true;
+      } else {
+          console.log ("Please enter the manager's name!");
+          return false; 
+      }
+  }
   },
   {
     type: "input",
-    name: "Manager's email",
+    name: "id",
+    message: "Please enter employee ID",
+    validate: nameInput => {
+      if  (isNaN(nameInput)) {
+          console.log ("Please enter the manager's ID!")
+          return false; 
+      } else {
+          return true;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "email",
     message: "Please enter the manager's email",
     validate: (answer) => {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -39,30 +55,37 @@ const teamQuestions = [
         return "You have to provide a valid email address!";
       }
       return true;
-    }
+    },
   },
   {
     type: "confirm",
     name: "add role",
     message: "do you want to add a new employee",
-    validate: (answer) => {
-      if (answer) {
-        inquirer.prompt({
-          type: "list",
-          name: "Roles",
-          message: "Please select the role you want to add to the team",
-          choices: roles,
-        })
-      }
-      return;
-    }
   },
-  
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Please enter the manager's office number",
+    validate: nameInput => {
+      if  (isNaN(nameInput)) {
+          console.log ('Please enter an office number!')
+          return false; 
+      } else {
+          return true;
+      }
+  }
+  },
+  {
+    type: "list",
+    name: "Roles",
+    message: "Please select the role you want to add to the team",
+    choices: roles,
+  },
 ];
 
 const promptTeamQuestions = () => {
   return inquirer.prompt(teamQuestions);
-    };
+};
 
 const init = async () => {
   const data = await promptTeamQuestions();
